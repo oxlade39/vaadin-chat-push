d6059bd84363de7851cb281d01ec95beec763a40
Dan Oxlade <dan.oxlade@gmail.com>
2013-05-08 08:08:27 +0100

    adding various cloud support

---
+++ src/main/bin/run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+java $JAVA_OPTS -classpath "vaadin-chat-push-1.0.0-SNAPSHOT/lib/*" com.github.oxlade39.vaadin.Main
--- src/main/java/com/github/oxlade39/vaadin/Main.java
+++ src/main/java/com/github/oxlade39/vaadin/Main.java
@@ -5,13 +5,21 @@ import com.vaadin.server.VaadinSession;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author dan
  */
 public class Main {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+    private static final String VMC_APP_PORT = "VMC_APP_PORT";
+
     public static void main(String[] args) throws Exception {
-        Server server = new Server(8080);
+        int port = resolvePort();
+        LOGGER.debug("resolved port to {}", port);
+        Server server = new Server(port);
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.setContextPath("/");
@@ -27,5 +35,13 @@ public class Main {
 
         server.start();
         server.join();
+    }
+
+    private static int resolvePort() {
+        String cloudfoundryPort = System.getenv().get(VMC_APP_PORT);
+        String herokuPort = System.getenv("PORT");
+        if(cloudfoundryPort != null) return Integer.parseInt(cloudfoundryPort);
+        else if(herokuPort != null) return Integer.parseInt(herokuPort);
+        else return 8080;
     }
 }
